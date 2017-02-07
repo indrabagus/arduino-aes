51346efb45e58e4a668019c41673e0a2fd072276
Indra Bagus <indra.bagus@gmail.com>
2017-02-07 22:58:08 +0700

    commit pertama kali

---
+++ Crypto.h
@@ -0,0 +1,31 @@
+#ifndef CRYPTO_H
+#define CRYPTO_H
+
+#include "Arduino.h"
+
+#define CRYPTO_CBC  0
+#define CRYPTO_ECB  1
+/*
+ * All the IV and Key variables should has 16 bytes in its length
+ * The padding use Zero padding
+ * Output buffer harus kelipatan 16 byte 
+*/
+class AES
+{
+    public:
+        AES();
+        void Init();
+        void SetIV(byte* piv); // Assume 16 byte
+        void SetKey(byte* pkey); // Assume 16 byte
+        void Encrypt(byte* pplaintext,int len,byte* poutput,int mode);
+        void Decrypt(byte* pciphertext, int len,byte* poutput,int mode);
+
+    private:
+        byte* m_piv;
+        byte* m_pkey;
+        void process(byte* pdatain,int len,byte* poutput,int mode,bool isencrypt);
+        void encryption(const byte* plaintext, const byte* key, byte* ciphered);
+        void decryption(const byte* ciphered, const byte* key,byte* plaintext);        
+};
+
+#endif
---
+++ FirmataCrypto.ino
@@ -0,0 +1,16 @@
+#include <Boards.h>
+#include <Firmata.h>
+
+#include "Crypto.h"
+
+AES aes;
+
+void setup() {
+  // put your setup code here, to run once:
+    aes.Init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
